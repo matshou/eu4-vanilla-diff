c2322c8f0a50f83d2d79b90839785f4515f0b47b
yooksi@zoho.com <yooksi@zoho.com>
2018-10-25 01:38:10 +0200

    :children_crossing: :heavy_plus_sign: Create batch script files

---
+++ vanilla-copy.bat
@@ -0,0 +1,15 @@
+@echo OFF
+
+set targetDir=%1
+echo Copying %targetDir% files...
+set src=%gamePath%\%targetDir%
+set dest=%rootPath%\%targetDir%
+IF not exist "%src%" goto notfound
+robocopy /s "%src%" "%dest%" /IT >> output_log.txt
+goto end
+
+:notfound
+echo [Error] Unable to find "%targetDir%" in game directory!
+echo.
+
+:end
---
+++ vanilla-update.bat
@@ -0,0 +1,43 @@
+@echo OFF
+echo.
+set rootPath=%cd%
+
+:readini
+
+IF not EXIST "vanilla.ini" (
+echo [Error] Missing configuration file, update your local repository.
+goto end
+)
+(
+set /p entry1=
+set /p gamePath=
+set /p entry2=
+set /p fileList=
+) < vanilla.ini
+
+IF not "%entry1%"=="gamePath =" (
+echo [Error] Missing 'gamePath' entry in config file!
+goto end
+)
+set gamePath=%gamePath:"=%
+IF not EXIST "%gamePath%\eu4.exe" (
+echo [Error] Invalid 'gamePath' entry in config file!
+goto end
+)
+IF not "%entry2%"=="fileList =" (
+echo [Error] Missing 'fileList' entry in configuration file!
+goto end
+)
+
+:copyfiles
+echo. > output_log.txt
+for %%a in (%fileList%) do ( CALL vanilla-copy.bat %%a )
+
+:complete
+echo.
+echo Completed copying vanilla files!
+echo Operation log saved in 'output_log.txt'
+goto end
+
+:end
+pause
