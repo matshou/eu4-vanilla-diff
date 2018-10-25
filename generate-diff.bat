f090416c067964ff7a31c9becfad7256b4f2e3b2
yooksi@zoho.com <yooksi@zoho.com>
2018-10-25 02:38:39 +0200

    :children_crossing: :heavy_plus_sign: Create diff script file

---
+++ generate-diff.bat
@@ -0,0 +1,14 @@
+@echo OFF
+CALL vanilla-update.bat -c
+git config --global core.safecrlf false > generate-diff.log
+echo Adding file contents to index...
+git add * >> generate-diff.log
+echo Recording changes to repository...
+git commit -m "Add temp vanilla files" >> generate-diff.log
+echo Writing diff to file...
+git diff --diff-filter=M master vanilla >> generate-diff.log
+echo Cleaning repository...
+git reset HEAD~ >> generate-diff.log
+git stash save --keep-index --include-untracked >> generate-diff.log
+git stash drop >> generate-diff.log
+pause
