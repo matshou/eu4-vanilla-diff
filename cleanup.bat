d619a0779b14c0cacfffb15554750fc6d695526f
yooksi@zoho.com <yooksi@zoho.com>
2018-10-25 07:04:14 +0200

    :zap: Add --no-reset argument

--- cleanup.bat
+++ cleanup.bat
@@ -1,26 +1,30 @@
 @echo OFF
 
+IF "%1"=="--no-reset" ( goto :clean )
+
+:input
 git log -1 --pretty=%%B > clean.tmp
 ( set /p commit= ) < clean.tmp
 IF not "%commit%"=="%commitMsg%" (
 	echo This script will drop your last commit and remove all untracked changes.
 	goto check
 )
-goto clean
+goto start
 
 :check
 set /p input="Are you sure you want to continue? (y/n): "
 IF "%input%"=="y" (
 	echo.
 	copy NUL build.log
-	goto clean
+	goto start
 )
 IF "%input%"=="n" ( goto pause )
 goto check
 
-:clean
+:start
 echo Resetting repository head...
 git reset HEAD~ >> build.log
+:clean
 echo Cleaning repository...
 git stash save --keep-index --include-untracked >> build.log
 git stash drop >> build.log
