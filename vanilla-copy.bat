036a4dda085188b6764f6dac7f6820b932ad8b6b
yooksi@zoho.com <yooksi@zoho.com>
2018-10-25 03:28:59 +0200

    :abc: Rename update output log

--- vanilla-copy.bat
+++ vanilla-copy.bat
@@ -5,7 +5,7 @@ echo Copying %targetDir% files...
 set src=%gamePath%\%targetDir%
 set dest=%rootPath%\%targetDir%
 IF not exist "%src%" goto notfound
-robocopy /s "%src%" "%dest%" /IT >> output_log.txt
+robocopy /s "%src%" "%dest%" /IT >> vanilla-update.log
 goto end
 
 :notfound
