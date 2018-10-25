5e37b8080d9b9443d2b750092f5e5f93d7414578
yooksi@zoho.com <yooksi@zoho.com>
2018-10-25 07:03:29 +0200

    :children_crossing: Add user message

--- commit.bat
+++ commit.bat
@@ -1,5 +1,20 @@
+@ECHO off
+
+IF NOT DEFINED commitMsg (
+	echo.
+	echo Commit message not defined, run build script.
+	goto pause
+)
+
 git config --global core.safecrlf false > build.log
 echo Adding file contents to index...
 git add * >> build.log
 echo Recording changes to repository...
 git commit -m "%commitMsg%" >> build.log
+goto end
+
+:pause
+echo.
+pause
+
+:end
